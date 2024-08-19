0465c02969e944090c9419213d0939503156243f
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-19 15:55:57 +0200

    staging - mail

--- dadi/script-dadi.js
+++ dadi/script-dadi.js
@@ -7,7 +7,6 @@ Assegnare un numero random al giocatore
 Assegnare un numero random alla CPU
 Fare il confronto per stabilire il vincitore e stampare il risultato
 
-
 */
 
 // # STAGING
@@ -17,7 +16,7 @@ const playerElement = document.getElementById('player');
 const cpuElement = document.getElementById('cpu');
 const winnerElement = document.getElementById('winner');
 
-//* Creare un array con i numeri di un dado
+//*  Creare un array con i numeri di un dado
 
 randomNumbers = [1, 2, 3, 4, 5, 6]
 
@@ -27,12 +26,12 @@ randomNumbers = [1, 2, 3, 4, 5, 6]
 
 // # PROCESSING
 
-//* Assegnare un numero random al giocatore
+// * Assegnare un numero random al giocatore
 
 const playerNumber = randomNumbers[Math.floor(Math.random() * 6)];
 console.log('Player', playerNumber)
 
-//* Assegnare un numero random alla CPU
+// * Assegnare un numero random alla CPU
 
 const cpuNumber = randomNumbers[Math.floor(Math.random() * 6)];
 console.log('CPU', cpuNumber)
@@ -40,7 +39,7 @@ console.log('CPU', cpuNumber)
 
 // # OUTPUT 
 
-//* Fare il confronto per stabilire il vincitore e stampare il risultato
+// * Fare il confronto per stabilire il vincitore e stampare il risultato
 
 let message = ' ';
 
