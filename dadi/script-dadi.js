970a8cc9b37a13eb03f69470b3a7d47d0fc60534
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-19 14:58:39 +0200

    Staging dadi

--- dadi/script-dadi.js
+++ dadi/script-dadi.js
@@ -1,9 +1,32 @@
 // PLANNING
 
-/* */
+/*
+Recupero gli elementi dal DOM
+Creare un array con i numeri dei dadi
+Assegnare un numero random al giocatore
+Assegnare un numero random alla CPU
+Fare il confronto per stabilire il vincitore
+Stampare in pagina
+
+*/
 
 // # STAGING
+
+// * Recupero gli elementi dal DOM
+const playerElement = document.getElementById("player");
+const cpuElement = document.getElementById("cpu");
+const winnerElement = document.getElementById("winner");
+
+
 // # DATA GATHERING
+
 // # EVENT HANDLING
+
 // # PROCESSING
+
+// * Creare un array con i numeri dei dadi
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+
 // # OUTPUT 
