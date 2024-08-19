e3590f9eb05c9d1c868daadafe48282eab33ce12
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-19 16:35:32 +0200

    Ciclo for - mail

--- mail/script-mail.js
+++ mail/script-mail.js
@@ -1,9 +1,52 @@
 // PLANNING
 
-/* */
+/*
+Creare un array con le email autorizzate
+Recuperare gli elementi dal DOM
+Confrontare la mail dell'utente con il mio elenco
+Comunicare l'esito
+ */
 
 // # STAGING
+
+// * Creare un array con le email autorizzate 
+
+authMails = [
+    "mariorossi01@gmail.com",
+    "pietrobianchi02@gmail.com",
+    "aldoverdi03@gmail.com",
+    "marcogialli04@gmail.com"
+]
+
+let message = '';
+
 // # DATA GATHERING
+
+// * Recuperare gli elementi dal DOM
+
+const resultElement = document.getElementById('result');
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
 // # EVENT HANDLING
+
+// * Confrontare la mail dell'utente con il mio elenco
+
+button.addEventListener('click', function () {
+    const inputValue = input.value.trim();
+
+    for (let i = 0; i < authMails.length; i++) {
+        let mail = authMails[i];
+
+        message = (inputValue === mail) ? "Accesso autorizzato" : "Accesso negato";
+    }
+    console.table(authMails)
+})
+
 // # PROCESSING
+
+
+
 // # OUTPUT 
+
+resultElement.innerText = message;
