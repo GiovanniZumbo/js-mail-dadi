27cf09f28eec95a8feaf6afc02e4eca86c947a33
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-20 11:02:43 +0200

    fixed mail

--- mail/script-mail.js
+++ mail/script-mail.js
@@ -9,16 +9,6 @@ Comunicare l'esito
 
 // # STAGING
 
-// * Creare un array con le email autorizzate 
-
-authMails = [
-    "mariorossi01@gmail.com",
-    "pietrobianchi02@gmail.com",
-    "aldoverdi03@gmail.com",
-    "marcogialli04@gmail.com"
-]
-
-let message = '';
 
 // # DATA GATHERING
 
@@ -28,32 +18,50 @@ const resultElement = document.getElementById('result');
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 
+// * Creare un array con le email autorizzate 
+
+const authMails = [
+    "mariorossi01@gmail.com",
+    "pietrobianchi02@gmail.com",
+    "aldoverdi03@gmail.com",
+    "marcogialli04@gmail.com"
+]
+
+let message = '';
+
 // # EVENT HANDLING
 
 // * Confrontare la mail dell'utente con il mio elenco
+
+/* Se avessimo preso il form, avremmo scritto 
+form.addEventListener ('submit', function (e) {
+e.preventDefault()
+})
+*/
 
 button.addEventListener('click', function () {
     const inputValue = input.value.trim();
 
     for (let i = 0; i < authMails.length; i++) {
 
-        let mail = authMails[i];
+        const mail = authMails[i];
 
-        message = (inputValue === mail) ? "Accesso autorizzato" : "Accesso negato";
+        const message = (inputValue === mail) ? "Accesso autorizzato" : "Accesso negato";
 
         console.log(i + " " + message); // funzionamento del ciclo in console
 
         if (authMails[i] === inputValue) {
             break;
-        } // Mi permette di fermare il ciclo for 
+        } // Mi permette di fermare il ciclo for (causa un piccolo leak di memoria)
 
+        //* Soluzione migliore del break: cambiare la condizione del for aggiungendo && !isAllowed
     }
 
     console.table(authMails)
 
+    // # OUTPUT 
+
     resultElement.innerText = message;
 })
 
-// # PROCESSING
 
-// # OUTPUT 
