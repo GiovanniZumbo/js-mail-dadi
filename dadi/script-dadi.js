66cfe8021b4b6beb224a84b925b7976c7642e757
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-20 09:55:02 +0200

    Fixed - dadi

--- dadi/script-dadi.js
+++ dadi/script-dadi.js
@@ -2,7 +2,6 @@
 
 /*
 Recupero gli elementi dal DOM
-Creare un array con i numeri di un dado
 Assegnare un numero random al giocatore
 Assegnare un numero random alla CPU
 Fare il confronto per stabilire il vincitore e stampare il risultato
@@ -15,41 +14,44 @@ Fare il confronto per stabilire il vincitore e stampare il risultato
 const playerElement = document.getElementById('player');
 const cpuElement = document.getElementById('cpu');
 const winnerElement = document.getElementById('winner');
+const button = document.querySelector('button');
 
-//*  Creare un array con i numeri di un dado
-
-randomNumbers = [1, 2, 3, 4, 5, 6]
 
 // # DATA GATHERING
 
 // # EVENT HANDLING
 
+button.addEventListener('click', function () {
+
+    const playerNumber = Math.floor(Math.random() * 6) + 1;
+    console.log('Player', playerNumber)
+
+    const cpuNumber = Math.floor(Math.random() * 6) + 1;
+    console.log('CPU', cpuNumber)
+
+    let message;
+
+    if (playerNumber === cpuNumber) {
+        message = 'C\'è stato un pareggio.'
+    } else {
+        message = (playerNumber > cpuNumber) ? "Hai vinto!" : "Hai perso.";
+    }
+
+    console.log(message);
+
+    playerElement.innerText = `Il tuo numero: ${playerNumber}`;
+    cpuElement.innerText = `Il mio numero: ${cpuNumber}`;
+    winnerElement.innerHTML = message;
+})
+
 // # PROCESSING
 
 // * Assegnare un numero random al giocatore
 
-const playerNumber = randomNumbers[Math.floor(Math.random() * 6)];
-console.log('Player', playerNumber)
 
 // * Assegnare un numero random alla CPU
 
-const cpuNumber = randomNumbers[Math.floor(Math.random() * 6)];
-console.log('CPU', cpuNumber)
 
-
-// # OUTPUT 
+// # OUTPUT
 
 // * Fare il confronto per stabilire il vincitore e stampare il risultato
-
-let message = ' ';
-
-if (playerNumber === cpuNumber) {
-    message = 'C\'è stato un pareggio.'
-} else {
-    message = (playerNumber > cpuNumber) ? "Hai vinto!" : "Hai perso.";
-}
-console.log(message);
-
-playerElement.innerText = `Il tuo numero: ${playerNumber}`;
-cpuElement.innerText = `Il mio numero: ${cpuNumber}`;
-winnerElement.innerHTML = message;
